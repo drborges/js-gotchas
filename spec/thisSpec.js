27d7ad2459c950610ff9a614f7c01a9462a75aaa
Diego Borges <dborges@rackspace.com>
2013-04-17 19:08:31 -0300

    Adds Perdido's challenge

--- spec/thisSpec.js
+++ spec/thisSpec.js
@@ -17,37 +17,78 @@ describe("'this' can be that, or not even that!", function() {
         throw 'Not yet implemented';
       });
     });
+  });
 
-    describe("Functions can have different 'these' depending on how you invoke them...", function() {
-      var partyFinder = {
-        partyPlace: 'in my pants, bitch!',
-        whereIsTheParty: function (name) {
-          return name + ' says: the party tonight is gonna be ' + this.partyPlace;
+  describe("Functions can have different 'these' depending on how you invoke them...", function() {
+    var partyFinder = {
+      partyPlace: 'in my pants, bitch!',
+    whereIsTheParty: function (name) {
+      return name + ' says: the party tonight is gonna be ' + this.partyPlace;
+    }
+    };
+
+    it("Normally invoking a function, i.e. fn()", function() {
+      expect(partyFinder.whereIsTheParty('Lipe')).toBe('Lipe says: the party tonight is gonna be in my pants, bitch!');
+    });
+
+    it("Invoking a function through fn.call(thisArg [, arg1 [, arg2 [, ...]]])", function() {
+      var context = { partyPlace: 'Onde mais Robin? eh claro que eh no ...!' };
+      var message = partyFinder.whereIsTheParty.call(context, 'Batman');
+      expect(message).toBe('Batman says: the party tonight is gonna be Onde mais Robin? eh claro que eh no ...!');
+    });
+
+    it("Invoking a function through fn.apply(thisArg [, args]) you get the same outcome as fn.call", function() {
+      var context = { partyPlace: 'Onde mais Robin? eh claro que eh no ...!' };
+      var message = partyFinder.whereIsTheParty.apply(context, ['Batman']);
+      expect(message).toBe('Batman says: the party tonight is gonna be Onde mais Robin? eh claro que eh no ...!');
+    });
+
+    it("You can also create a new function binding a brand new 'context' to it using fn.bind(thisArg)", function() {
+      var context = { partyPlace: 'at CabareEeEeEet!' };
+      var brandNewPartyFinder = partyFinder.whereIsTheParty.bind(context);
+      var message = brandNewPartyFinder('Dani');
+      expect(message).toBe('Dani says: the party tonight is gonna be at CabareEeEeEet!');
+    });
+  });
+
+  describe("Please fix me!! <o>", function() {
+    var o;
+
+    beforeEach(function() {
+      spyOn(console, 'log');
+
+      o = {
+        f1: function() {
+          console.log('hi');
+        },
+        f2: function() {
+          this.f1();
         }
       };
 
-      it("Normally invoking a function, i.e. fn()", function() {
-        expect(partyFinder.whereIsTheParty('Lipe')).toBe('Lipe says: the party tonight is gonna be in my pants, bitch!');
-      });
+      o.f2.prototype.f1 = function() {
+       console.log('bye');
+      }
+    });
 
-      it("Invoking a function through fn.call(thisArg [, arg1 [, arg2 [, ...]]])", function() {
-        var context = { partyPlace: 'Onde mais Robin? eh claro que eh no ...!' };
-        var message = partyFinder.whereIsTheParty.call(context, 'Batman');
-        expect(message).toBe('Batman says: the party tonight is gonna be Onde mais Robin? eh claro que eh no ...!');
-      });
+    it("what does console.log outputs when: new o.f2()", function() {
+      new o.f2();
+      throw 'Implement me!'
+    });
 
-      it("Invoking a function through fn.apply(thisArg [, args]) you get the same outcome as fn.call", function() {
-        var context = { partyPlace: 'Onde mais Robin? eh claro que eh no ...!' };
-        var message = partyFinder.whereIsTheParty.apply(context, ['Batman']);
-        expect(message).toBe('Batman says: the party tonight is gonna be Onde mais Robin? eh claro que eh no ...!');
-      });
+    it("what does console.log outputs when: o.f2.apply(k)", function() {
+      var k = {
+        f1: function() {
+          console.log('pfff');
+        }
+      }
+      o.f2.apply(k);
+      throw 'Implement me!'
+    });
 
-      it("Another way of changing the semantics of 'this' is to create a new function with a brand new 'context' using fn.bind(thisArg)", function() {
-        var context = { partyPlace: 'at CabareEeEeEet!' };
-        var brandNewPartyFinder = partyFinder.whereIsTheParty.bind(context);
-        var message = brandNewPartyFinder('Dani');
-        expect(message).toBe('Dani says: the party tonight is gonna be at CabareEeEeEet!');
-      });
+    it("what does console.log outputs when: o.f2();", function() {
+      o.f2();
+      throw 'Implement me!'
     });
   });
 });
